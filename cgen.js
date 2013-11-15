ec9c91a6603104c53c97a6c5cfcffc08680a6ec3
toki.migimatsu <toki.migimatsu@gmail.com>
2013-11-15 00:48:07 -0800

    cleaned files

--- cgen.js
+++ cgen.js
@@ -1,4 +1,4 @@
-var types = ['maj', 'min', 'dim', 'aug', 'maj7', 'min7'];
+var types = ['Maj', 'Min', 'Dim', 'Aug', 'Maj7', 'Min7'];
 var NUM_NOTES = 12;
 var notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 var NUM_OCTAVES = 5;
@@ -7,8 +7,7 @@ var NUM_OCTAVES = 5;
 //[C,E,G], this will return [[C, E, G], [E, G, C], and [G, C, E]] 
 function generateAllInversions(tonic){
 	var all = [];
-	var len = tonic.length;
-	for (var i = 0; i < len; i++){
+	for (var i = 0; i < tonic.length; i++){
 		all.push(tonic.slice(0));//move first element to last
 		tonic.move(0,-1);
 		tonic[tonic.length-1] += 12;
@@ -38,7 +37,16 @@ function generateBase(rootStr, typeStr){
 	var type = types.indexOf(typeStr);
 	var root = notes.indexOf(rootStr);
 	var result = [];
-	var c = { 'root' : root, 'majthird': (root + 4)%NUM_NOTES, 'minthird': (root + 3)%NUM_NOTES, 'perfectfifth' :(root + 7)%NUM_NOTES, 'dimfifth':(root + 6)%NUM_NOTES, 'augfifth':(root + 8)%NUM_NOTES, 'majorseventh':(root + 11)%NUM_NOTES , 'minorseventh': (root + 10)%NUM_NOTES};
+	var c = {
+		'root' : root,
+		'majthird': (root + 4)%NUM_NOTES,
+		'minthird': (root + 3)%NUM_NOTES,
+		'perfectfifth' :(root + 7)%NUM_NOTES,
+		'dimfifth':(root + 6)%NUM_NOTES,
+		'augfifth':(root + 8)%NUM_NOTES,
+		'majorseventh':(root + 11)%NUM_NOTES,
+		'minorseventh': (root + 10)%NUM_NOTES
+	};
 	result.push(c.root);
 
 	switch(type)
@@ -81,7 +89,7 @@ function outputResults(root, type, results, start, end){
 	var resultsStr = [];
 	for(var i = 0; i < results.length; i++){
 		for(var j =0; j< results[i].length; j++){
-			resultsStr.push({name:(i+start)+root+type+j, chord:convertToString(results[i][j])});	
+			resultsStr.push({name:(i+start)+root+type+j, chord:convertToString(results[i][j])});
 		}
 	}
 	return resultsStr;
@@ -126,7 +134,7 @@ ChordGenerator = function(note, type, startoctave, endoctave) {
 			});
 		});
 	} else {
-		output.push(generateChord(note, type, parseInt(startoctave), parseInt(endoctave)));
+		output.push(generateChord(note, type, parseInt(startoctave, 10), parseInt(endoctave, 10)));
 	}
 	return output;
 };
