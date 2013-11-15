5aa5b7732e58093b1ce22913f6f16d9308640dd0
Toki Migimatsu <takatoki@corn02.stanford.edu>
2013-11-15 10:25:36 -0800

    removes duplicate samp batch

--- convert_midi2wav.sh
+++ convert_midi2wav.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+if [[ -z $1 ]]; then
+	echo "Need to specify folder!"
+	exit 1
+fi
+
 mkdir ${1}wav
 for sfile in midi_files/soundfonts/*.sf2
 do
 	dir=${1}wav/${sfile##*/}
 	dir=${dir%.*}
 	mkdir $dir
+	touch ${dir}/files.txt
 	for file in $1*.mid
 	do
 		ofile=${file/%mid/wav}
-		ofile=${dir}/${ofile##*/}
-		FluidSynth/fluidsynth -F $ofile -i -n -T wav $sfile $file
+		ofile=${ofile##*/}
+		FluidSynth/fluidsynth -F ${dir}/${ofile} -i -n -T wav $sfile $file
+		echo "$ofile" >> ${dir}/files.txt
 	done
 done
