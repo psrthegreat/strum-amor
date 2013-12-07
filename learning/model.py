7db8b225e9cdc54068ab795808e34f068442a1e1
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2013-12-07 10:55:05 -0800

    implements logisreg on model.py

--- learning/model.py
+++ learning/model.py
@@ -15,7 +15,7 @@ class SVM(object):
           SVM classifier.
 
     """
-    def __init__(self, **svm_args):
+    def __init__(self, **model_args):
         """
         Initializes model with parameters for underlying sklearn.svm.SVC class. 
 
@@ -44,11 +44,46 @@ class SVM(object):
         return self.svm.score(frames, labels)
 
 class Softmax(object):
+
+    from sklearn.linear_model import LogisticRegression
+    
     """
     Softmax regression.
 
+    Uses Scikit Logistic Regression
+    Usage Example: Softmax(C=C, tol=0.01)
     """
-    pass
+    def __init__(self, **model_args):
+
+        """
+        Initializes model with parameters for underlying sklearn.svm.SVC class. 
+
+        """
+        self.softmax = LogisticRegression(**model_args)
+
+
+
+    def fit(self, frames, labels):
+        self.softmax.fit(frames, labels);
+        """
+        Fits SVM with labeled frame data.
+
+        """
+        self.softmax.fit(frames, labels)
+
+    def predict(self, frames):
+        """
+        Predicts chords for frames.
+
+        """
+        return self.softmax.predict(frames)
+
+    def score(self, frames, labels):
+        """
+        Returns accuracy of predictions on frames.
+
+        """
+        return self.softmax.score(frames, labels)
 
 class GaussianMixture(object):
     """
