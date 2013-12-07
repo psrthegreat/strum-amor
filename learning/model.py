b7f0ed042911dce74932ffbdb630eac109a18288
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2013-12-07 11:07:02 -0800

    implements inheritance on model.py

--- learning/model.py
+++ learning/model.py
@@ -4,88 +4,61 @@ models use single frame vectors as features, predicting a chord
 for a frame from a vector of features.
 
 """
-from sklearn import svm
-
-class SVM(object):
-    """
-    Use SVM to predict chords.
-
-    Member variables:
-      svm: sklearn.svm.SVC
-          SVM classifier.
-
-    """
+class Model(object):
+    #initializes with model parameters
     def __init__(self, **model_args):
-        """
-        Initializes model with parameters for underlying sklearn.svm.SVC class. 
+        pass
 
-        """
+    #Fits the model to data
+    def fit(self, frames, labels):
+        pass
+
+    #Predicts outputs for models
+    def predict(self, frames):
+        pass
+
+    #returns mean accuracy of predictions on frames
+    def score(self, frames, labels):
+        return self.svm.score(frames, labels)
+
+
+class SVM(Model):
+    from sklearn import svm
+    #Usage Example: 
+    def __init__(self, **model_args):
         self.svm      = svm.SVC(**svm_args) 
 
     def fit(self, frames, labels):
-        """
-        Fits SVM with labeled frame data.
-
-        """
         self.svm.fit(frames, labels)
 
     def predict(self, frames):
-        """
-        Predicts chords for frames.
-
-        """
         return self.svm.predict(frames)
 
     def score(self, frames, labels):
-        """
-        Returns accuracy of predictions on frames.
-
-        """
         return self.svm.score(frames, labels)
 
-class Softmax(object):
-
+class Softmax(Model):
     from sklearn.linear_model import LogisticRegression
-    
-    """
-    Softmax regression.
-
-    Uses Scikit Logistic Regression
-    Usage Example: Softmax(C=C, tol=0.01)
-    """
+    #Usage Example: Softmax(C=C, tol=0.01)
     def __init__(self, **model_args):
-
-        """
-        Initializes model with parameters for underlying sklearn.svm.SVC class. 
-
-        """
         self.softmax = LogisticRegression(**model_args)
-
-
 
     def fit(self, frames, labels):
         self.softmax.fit(frames, labels);
-        """
-        Fits SVM with labeled frame data.
-
-        """
-        self.softmax.fit(frames, labels)
-
+       
     def predict(self, frames):
-        """
-        Predicts chords for frames.
-
-        """
         return self.softmax.predict(frames)
 
     def score(self, frames, labels):
-        """
-        Returns accuracy of predictions on frames.
-
-        """
         return self.softmax.score(frames, labels)
 
-class GaussianMixture(object):
+    """ Returns the probability of the sample for each class in the model
+        [n_samples, n_classes]
+    """
+    def probs(self, frames):
+        return self.softmax.predict_proba(frames) 
+
+class GaussianMixture(Model):
     """
     Gaussian mixture.
     """
