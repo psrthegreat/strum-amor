7a7e07800f164b6ba4e8a6ab47474970df6910f0
Brad Girardeau <brad.girardeau@gmail.com>
2013-12-09 20:53:53 -0800

    Clean up ipc feature extraction.

--- learning/feature.py
+++ learning/feature.py
@@ -1,10 +1,53 @@
+"""
+Facilitates efficient feature extraction with Matlab bridge.
+
+Run as script to start server. Import and use extraction methods
+as clients.
+
+"""
 import os
-from multiprocessing.connection import Client
+
+import ipc
+
+def load_matlab():
+    """
+    Imports and starts matlab bridge if not started.
+
+    """
+    global mlab
+    global MatlabError
+
+    from mlabwrap import mlab
+    from mlabraw import error as MatlabError
+
+    mlab.addpath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "features/matlab-chroma-toolbox"))
+
+
+def fetch_data(command):
+    """
+    Fetches data based on command received by client.
+
+    """
+    dir, file = os.path.split(command)
+    try:
+        return mlab.extract_chord_features(dir + "/", file, 0, "").T
+    except MatlabError as e:
+        return "Error: %s" %(str(e))
 
 def get_chroma(path):
-    address = ("localhost", 7000)
-    connection = Client(address, authkey='strumamor')
+    """
+    Extract chroma.
+
+    """
+    connection = ipc.get_client()
+
     connection.send(os.path.abspath(path))
+
     data = connection.recv()
     connection.close()
+
     return data
+
+if '__main__' in __name__:    
+    load_matlab()
+    ipc.run_server(fetch_data)
