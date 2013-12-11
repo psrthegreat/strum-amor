53bb824ae3d0c4778b8aa734ed0c89f2eb357a1a
toki.migimatsu <toki.migimatsu@gmail.com>
2013-12-10 20:46:17 -0800

    update trainsave, new train/test lists

--- learning/trainsave.py
+++ learning/trainsave.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.argv = ["train.py", "../features/output/Piano_1/crp", "f_CRP"]
+sys.argv = ["train.py", "../features/output/Piano_1/crp/22050", "crp"]
 execfile("train.py")
 model = SegmentHMM(HMMGaussian())
 model.train([xtrain], [ytrain])
